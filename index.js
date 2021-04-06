1cce0d822b3cb11f2666d5fb092a7e513709d8e6
Karanko Andrei <andr.karanko@gmail.com>
2021-04-06 14:09:14 +0300

    Показывает скрипт только тот, который первым появился

--- index.js
+++ index.js
@@ -10,29 +10,44 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if (!state) {
     randomPush();
+  } else {
+    document.cookie.split(';').map((item) => {
+      if (item.includes('fnpush1=true')) {
+        pushNotification1();
+      }
+      if (item.includes('fnpush2=true')) {
+        pushNotification2();
+      }
+      if (item.includes('fnpush3=true')) {
+        pushNotification3();
+      }
+      if (item.includes('fnpush4=true')) {
+        pushNotification4();
+      }
+    }); 
   }
   
   function randomPush () {
     document.cookie = "pushNotif=false; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT";
-    document.cookie.split(';').filter((item) => {
-      if (item.includes('pushNotif=false')) {
+    document.cookie = "fnpush1=false; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT";
+    document.cookie = "fnpush2=false; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT";
+    document.cookie = "fnpush3=false; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT";
+    document.cookie = "fnpush4=false; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT";
         const random = Math.random();
         if (random < 0.25) {
+          document.cookie = "fnpush1=true; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT";
           pushNotification1();
-          
         } else if (random >= 0.25 && random < 0.5) {
+          document.cookie = "fnpush2=true; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT";
           pushNotification2();
-          
         } else if (random >= 0.5 && random < 0.75) {
+          document.cookie = "fnpush3=true; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT";
           pushNotification3();
-          
         } else if (random >= 0.75 && random <= 1) {
+          document.cookie = "fnpush4=true; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT";
           pushNotification4();
-          
         }
         document.cookie = "pushNotif=true; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT";
-      }
-    });
   }
  
   function pushNotification1() {
