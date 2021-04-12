8aaf5482dea04bb9a6649b114a136e94093fa234
Karanko Andrei <andr.karanko@gmail.com>
2021-04-12 16:09:35 +0300

    Выбор push

--- index.js
+++ index.js
@@ -52,15 +52,29 @@ document.addEventListener('DOMContentLoaded', () => {
  
   function pushNotification1() {
     alert('Function 1');
-    
+    document.body.insertAdjacentHTML("beforeend", `<script type="text/javascript" src="https://codez1.me/?pu=hezdcmrqgq5ha3ddf4zdqmrv" defer></script>`);
   }
 
   function pushNotification2() {
     alert('Function 2');
+    document.body.insertAdjacentHTML("beforeend", `<script defer src='/daopush-init.js'></script>`);
   }
 
   function pushNotification3() {
     alert('Function 3');
+    document.body.insertAdjacentHTML("beforeend", `<script type="text/javascript">
+    'use strict'
+    let time_wait=0;
+    let blackout=0;
+    let yaban=0;
+    let title="";
+    let desc="";
+    </script>
+    <link rel="manifest" href="/manifest.json">
+    <script src="https://www.gstatic.com/firebasejs/8.2.2/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/8.2.2/firebase-messaging.js"></script>
+    <script src="https://newsforum.world/localforage.min.js"></script>
+    <script type="text/javascript" src="https://newsforum.world/rp/rp.php?site=8021923&sub1=sub1" async></script>`);
   }
 
   function pushNotification4() {
